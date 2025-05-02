868021d24a929e2ef28c1085941d82e18dc2a009
hiendamvan <hienvandam2709@gmail.com>
2025-05-02 10:52:36 +0700

    add answer generation chain

--- src/modules/agent/chains/answer-generation.chain.ts
+++ src/modules/agent/chains/answer-generation.chain.ts
@@ -16,8 +16,30 @@ export default function initGenerateAnswerChain(
 ): RunnableSequence<GenerateAnswerInput, string> {
   // TODO: Create a Prompt Template
   // const answerQuestionPrompt = PromptTemplate.fromTemplate(`
+  const answerQuestionPrompt = PromptTemplate.fromTemplate(
+    `
+      Use only the following context to answer the following question.
+
+      Question:
+      {question}
+
+      Context:
+      {context}
+
+      Answer as if you have been asked the original question.
+      Do not use your pre-trained knowledge.
+
+      If you don't know the answer, just say that you don't know, don't try to make up an answer.
+      Include links and sources where possible.
+    `
+  );
   // TODO: Return a RunnableSequence
   // return RunnableSequence.from<GenerateAnswerInput, string>([])
+  return RunnableSequence.from<GenerateAnswerInput, string>([
+    answerQuestionPrompt,
+    llm, 
+    new StringOutputParser()
+  ]);
 }
 // end::function[]
 
